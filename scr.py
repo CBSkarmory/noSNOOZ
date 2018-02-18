e9aed65e56a5c548d1e346c7648de3b053c5227a
CBSkarmory <CBSkarmory@users.noreply.github.com>
2018-02-17 21:43:23 -0500

    refactoring

--- scr.py
+++ scr.py
@@ -7,12 +7,15 @@ sub = reddit.subreddit("news+worldnews")
 
 DEBUG = True
 
+
 def dout(message):
     if DEBUG:
         print(f"[DEBUG] {message}")
 
+
 def current_sec_time():
     return int(round(time.time()))        
+
 
 def print_col(collection):
     for post in collection:
@@ -32,33 +35,12 @@ def get_top_hour_n(n):
 def get_rising_n(n):
     return sub.rising(limit=n)
 
-# # 3 level deep sum of top comments
-# def sum3(post):
-#     return comment_sum(post.comments, 3)
-
-# def comment_sum(comment_forest, levels):
-#     if levels == 0:
-#         return 0
-#     val, idx = get_max_pos(comment_forest)
-#     return val + comment_sum(comment_forest[idx], levels - 1)
-    
-# def get_max_pos(comment_forest):
-#     max_val = -1
-#     max_idx = -1
-#     for i in range(len(comment_forest)):
-#         if comment_forest[i].score > max_val:
-#             max_val = comment_forest[i].score
-#             max_idx = i
-#     return (max_val, max_idx)
 
 def max_score(comments):
-    max_comment = None
-    max_score = 0
+    max_val = 0
     for com in comments.list():
-        if com.score > max_score:
-            max_score = com.score
-            max_comment = com
-    return max_score
+        max_val = max(max_val, com.score)
+    return max_val
     
 
 def get_top_hr_as_df(n):
