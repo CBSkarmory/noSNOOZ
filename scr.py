294dfad2bb336d6521e4aa2a2344ede8b9efcfa7
CBSkarmory <CBSkarmory@users.noreply.github.com>
2018-02-17 21:35:56 -0500

    script data processing

--- scr.py
+++ scr.py
@@ -32,10 +32,34 @@ def get_top_hour_n(n):
 def get_rising_n(n):
     return sub.rising(limit=n)
 
-def sum3():
-    # TODO implement me
-    return -1
+# # 3 level deep sum of top comments
+# def sum3(post):
+#     return comment_sum(post.comments, 3)
 
+# def comment_sum(comment_forest, levels):
+#     if levels == 0:
+#         return 0
+#     val, idx = get_max_pos(comment_forest)
+#     return val + comment_sum(comment_forest[idx], levels - 1)
+    
+# def get_max_pos(comment_forest):
+#     max_val = -1
+#     max_idx = -1
+#     for i in range(len(comment_forest)):
+#         if comment_forest[i].score > max_val:
+#             max_val = comment_forest[i].score
+#             max_idx = i
+#     return (max_val, max_idx)
+
+def max_score(comments):
+    max_comment = None
+    max_score = 0
+    for com in comments.list():
+        if com.score > max_score:
+            max_score = com.score
+            max_comment = com
+    return max_score
+    
 
 def get_top_hr_as_df(n):
     top_n = get_top_hour_n(n)
@@ -45,8 +69,9 @@ def get_top_hr_as_df(n):
     df['score'] = df.post.apply(lambda post: post.score)
     df['top_lv_c'] = df.post.apply(lambda post: len(post.comments))
     df['seconds_old'] = df.post.apply(lambda post: int(current_sec_time() - post.created_utc))
-    df['metric'] = df.post.apply(sum3)
+    df['max_c_score'] = df.post.apply(lambda post: max_score(post.comments))
     return df
+
 
 if __name__ == "__main__":
     n= 7
