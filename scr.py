4a1981ba264c536bebcd01d77fb627f8dc2453ce
CBSkarmory <CBSkarmory@users.noreply.github.com>
2018-02-17 22:38:11 -0500

    data processing

--- scr.py
+++ scr.py
@@ -32,6 +32,10 @@ def get_top_hour_n(n):
     return sub.top(time_filter='hour',limit=n)
 
 
+def get_hot_n(n):
+    return sub.hot(limit=n)
+
+
 def get_rising_n(n):
     return sub.rising(limit=n)
 
@@ -39,23 +43,35 @@ def get_rising_n(n):
 def max_score(comments):
     max_val = 0
     for com in comments.list():
+        if isinstance(com, praw.models.MoreComments):
+            continue
         max_val = max(max_val, com.score)
     return max_val
-    
 
-def get_top_hr_as_df(n):
-    top_n = get_top_hour_n(n)
+
+def posts_as_df(posts):
     df = pd.DataFrame({
-        'post': [post for post in top_n]
+        'post': [post for post in posts]
     })
     df['score'] = df.post.apply(lambda post: post.score)
     df['top_lv_c'] = df.post.apply(lambda post: len(post.comments))
     df['seconds_old'] = df.post.apply(lambda post: int(current_sec_time() - post.created_utc))
     df['max_c_score'] = df.post.apply(lambda post: max_score(post.comments))
+    # these are times 1000
+    df['dp_dt'] = (df.score * 1000.0 * 60.0 / df.seconds_old).apply(int)
+    df['d2p_dt2'] = (df.dp_dt * 60.0 / df.seconds_old).apply(int)
     return df
+
+
+def public_get_predictions():
+    # TODO implement me
+    return [list(get_rising_n(1))[0]]
 
 
 if __name__ == "__main__":
     n= 7
     #print_col(set(get_rising_n(n)) & set(get_top_hour_n(n)))
-    print(get_top_hr_as_df(5))
+    print("hot")
+    print(posts_as_df(get_hot_n(5)))
+    print("intersect")
+    print(posts_as_df(set(get_rising_n(8)) & set(get_top_hour_n(8))))
